e1fd17f8f4a51dc151ae8e3c988f35e3037a4ca6
yusei53 <yusei530922@gmail.com>
2025-07-13 16:14:47 +0900

    clean up ec2 instance

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ variable "env" {
   default = "dev"
 }
 
-variable "myip" {
-  type = string
-  description = "Check-> https://www.whatismyip.com/"
-}
+# variable "myip" {
+#   type = string
+#   description = "Check-> https://www.whatismyip.com/"
+# }
 
 resource "aws_vpc" "web_vpc" {
   cidr_block = "10.0.0.0/16"
@@ -35,80 +35,80 @@ resource "aws_subnet" "web_subnet" {
   }
 }
 
-resource "aws_route_table" "web_public_rtb" {
-  vpc_id = aws_vpc.web_vpc.id
+# resource "aws_route_table" "web_public_rtb" {
+#   vpc_id = aws_vpc.web_vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.web_igw.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.web_igw.id
+#   }
 
-  tags = {
-    Name = "${local.name_prefix}-public-rtb"
-  }
-}
+#   tags = {
+#     Name = "${local.name_prefix}-public-rtb"
+#   }
+# }
 
-resource "aws_route_table_association" "web_public_rtb_assoc" {
-  subnet_id      = aws_subnet.web_subnet.id
-  route_table_id = aws_route_table.web_public_rtb.id
-}
+# resource "aws_route_table_association" "web_public_rtb_assoc" {
+#   subnet_id      = aws_subnet.web_subnet.id
+#   route_table_id = aws_route_table.web_public_rtb.id
+# }
 
-resource "aws_internet_gateway" "web_igw" {
-  vpc_id = aws_vpc.web_vpc.id
+# resource "aws_internet_gateway" "web_igw" {
+#   vpc_id = aws_vpc.web_vpc.id
 
-  tags = {
-    Name = "${local.name_prefix}-igw"
-  }
-}
+#   tags = {
+#     Name = "${local.name_prefix}-igw"
+#   }
+# }
 
-resource "aws_security_group" "web_sg" {
-  vpc_id = aws_vpc.web_vpc.id
+# resource "aws_security_group" "web_sg" {
+#   vpc_id = aws_vpc.web_vpc.id
 
-  name        = "${local.name_prefix}-sg"
-  description = "Allow HTTP access from my IP"
+#   name        = "${local.name_prefix}-sg"
+#   description = "Allow HTTP access from my IP"
 
-  ingress {
-    description = "Allow HTTP traffic from my IP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["${var.myip}/32"] # var.myipからのHTTPアクセスを許可
-  }
+#   ingress {
+#     description = "Allow HTTP traffic from my IP"
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["${var.myip}/32"] # var.myipからのHTTPアクセスを許可
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "${local.name_prefix}-sg"
-  }
-}
+#   tags = {
+#     Name = "${local.name_prefix}-sg"
+#   }
+# }
 
-resource "aws_instance" "web_ec2" {
-  ami                         = "ami-094dc5cf74289dfbc" 
-  instance_type               = "t2.micro"
-  security_groups             = [aws_security_group.web_sg.id]
-  subnet_id = aws_subnet.web_subnet.id
+# resource "aws_instance" "web_ec2" {
+#   ami                         = "ami-094dc5cf74289dfbc" 
+#   instance_type               = "t2.micro"
+#   security_groups             = [aws_security_group.web_sg.id]
+#   subnet_id = aws_subnet.web_subnet.id
 
-  user_data = <<-EOF
-#!/bin/bash
-dnf update -y
-dnf install -y nginx
-systemctl enable --now nginx
-cat <<HTML > /usr/share/nginx/html/index.html
-    <div style="text-align:center; font-size:1.5em; color:#333; margin:20px; line-height:1.8;">
-        <b>env: ${var.env}</b><br>
-        <b>app_name: ${local.app_name}</b><br>
-        <b>name_prefix: ${local.name_prefix}</b><br>
-        <b>myip: ${var.myip}</b>
-    </div>
-HTML
-  EOF
+#   user_data = <<-EOF
+# #!/bin/bash
+# dnf update -y
+# dnf install -y nginx
+# systemctl enable --now nginx
+# cat <<HTML > /usr/share/nginx/html/index.html
+#     <div style="text-align:center; font-size:1.5em; color:#333; margin:20px; line-height:1.8;">
+#         <b>env: ${var.env}</b><br>
+#         <b>app_name: ${local.app_name}</b><br>
+#         <b>name_prefix: ${local.name_prefix}</b><br>
+#         <b>myip: ${var.myip}</b>
+#     </div>
+# HTML
+#   EOF
 
-  tags = {
-    Name = "${local.name_prefix}-ec2"
-  }
-}
+#   tags = {
+#     Name = "${local.name_prefix}-ec2"
+#   }
+# }
