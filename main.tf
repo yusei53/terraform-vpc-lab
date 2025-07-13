50dc21070e2cc9a3cda522447bf657f17eb77e51
yusei53 <yusei530922@gmail.com>
2025-07-13 15:55:13 +0900

    feat: web-vpc

--- main.tf
+++ main.tf
@@ -2,17 +2,26 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_vpc" "terraform_vpc" {
+locals {
+  app_name = "web"
+}
+
+variable "env" {
+  type = string
+  default = "dev"
+}
+
+resource "aws_vpc" "web_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "aws_vpc_name_first"
+    Name = "${var.env}-${local.app_name}-vpc"
   }
 }
 
-resource "aws_subnet" "terraform_subnet" {
-  vpc_id = aws_vpc.terraform_vpc.id
+resource "aws_subnet" "web_subnet" {
+  vpc_id = aws_vpc.web_vpc.id
   cidr_block = "10.0.1.0/24"
   tags = {
-    Name = "aws_subnet_name_first"
+    Name = "${var.env}-${local.app_name}-subnet"
   }
 }
