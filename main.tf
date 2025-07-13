0022ec95188376a78edae9b61a3918c773b8d3e7
yusei53 <yusei530922@gmail.com>
2025-07-13 15:34:53 +0900

    feat: aws_subnet

--- main.tf
+++ main.tf
@@ -8,3 +8,11 @@ resource "aws_vpc" "terraform_vpc" {
     Name = "aws_vpc_name_first"
   }
 }
+
+resource "aws_subnet" "terraform_subnet" {
+  vpc_id = aws_vpc.terraform_vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "aws_subnet_name_first"
+  }
+}
